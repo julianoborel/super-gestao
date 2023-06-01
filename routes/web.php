9348725c05e365a034b5ce074b4b28da70cdb32a
Juliano Borel <julianoborel@yahoo.com.br>
2023-06-01 00:00:48 -0300

    Adição de novas rotas para fins de teste e agrupamento com prefixo das mesmas

--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,10 @@ Route::get('/', 'PrincipalController@principal');
 Route::get('/contato', 'ContatoController@contato');
 
 Route::get('/sobre-nos', 'SobreNosController@sobreNos');
+Route::get('/login', function(){ return 'Login'; });
 
-Route::get(
-    '/contato/{nome}/{categoria_id}',
-    function(
-        string $nome = 'Desconhecido',
-        int $categoria_id = 1) {
-        echo "Estamos aqui: $nome - $categoria_id";
-    }
-)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function(){ return 'Clientes'; });
+    Route::get('/fornecedores', function(){ return 'Fornecedores'; });
+    Route::get('/produtos', function(){ return 'Produtos'; });
+});
